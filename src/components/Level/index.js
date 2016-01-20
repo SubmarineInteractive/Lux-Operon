d20fba70df858f39124f4b02327691bdd6c0ffa5
Patrick HENG <hengpatrick.pro@gmail.com>
2016-01-20 12:10:55 +0100

    WebGl - Start level component

---
+++ src/components/Level/index.js
@@ -0,0 +1,17 @@
+/**
+ * Level class
+ */
+class Level extends THREE.Object3D {
+
+  /**
+   * Constructor function
+   * @param  {object} options Options
+   * @return {void}
+   */
+  constructor() {
+    super()
+  }
+
+}
+
+export default Level
---
+++ src/config/levels.js
@@ -0,0 +1,9 @@
+export default [
+	{
+		name: "level 1",
+	},	{
+		name: "level 2",
+	},	{
+		name: "level 3",
+	}
+]
