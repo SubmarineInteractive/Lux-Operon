d07de116b0de50bfdea464a60b4e3ca505ac9263
Patrick HENG <hengpatrick.pro@gmail.com>
2016-01-26 16:59:00 +0100

    WebGL - Add ambient light

---
+++ src/components/WebGL/Light/AmbientLight.js
@@ -0,0 +1,25 @@
+import Container from 'Container';
+
+/**
+ * AmbientLight class
+ */
+class AmbientLight extends THREE.AmbientLight {
+
+  /**
+   * Constructor function
+   * @param {Configuration} Configuration instance
+   * @param {Level} Level instance
+   */
+  constructor(Configuration, Level) {
+    const lightColor = Configuration.get('lights').ambientLight.color;
+
+    super(lightColor);
+
+    this.level = Level;
+
+    Container.get('Scene').add(this);
+  }
+
+}
+
+export default AmbientLight;
---
+++ src/config/lights.js
@@ -0,0 +1,5 @@
+export default {
+  "ambiantLight" : {
+    color: 0x027fc1
+  }
+};
