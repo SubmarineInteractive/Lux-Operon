d3c96747ed42f570700d0bf42080717e91e686fa
Patrick HENG <hengpatrick.pro@gmail.com>
2016-01-26 17:24:51 +0100

    WebGL - Improve terrain (options / dependancies)

--- src/components/WebGL/Terrain/TerrainGeometry.js
+++ src/components/WebGL/Terrain/TerrainGeometry.js
@@ -6,8 +6,10 @@ class TerrainGeometry extends THREE.PlaneBufferGeometry {
   /**
    * Constructor function
    */
-  constructor() {
-    super(2000, 2000, 32, 32);
+  constructor(Configuration) {
+    const terrainGeomConfig = Configuration.get('terrain.geometry');
+
+    super(terrainGeomConfig.width,terrainGeomConfig.depth, terrainGeomConfig.segments.width, terrainGeomConfig.segments.depth);
 
     this.computeFaceNormals();
     this.computeVertexNormals();
---
+++ src/config/terrain.js
@@ -0,0 +1,10 @@
+export default {
+  "geometry" : {
+    "width": 2000,
+    "depth": 2000,
+    "segments": {
+      "width": 32,
+      "depth": 32
+    }
+  }
+}
