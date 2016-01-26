26e25f809d5f45c31d6d76ba1308250a594bfb78
Fabien Motte <contact@fabienmotte.com>
2016-01-26 11:23:20 +0100

    WebGL - Add terrain textures

--- src/config/textures.js
+++ src/config/textures.js
@@ -1,3 +1,6 @@
 export default {
-  
+  "heightMap": "./textures/heightmap.png",
+  "normalMap": "./textures/normalmap.png",
+  "diffuseTexture1": "./textures/diffuseTexture1.jpg",
+  "detailTexture": "./textures/detailTexture.jpg"
 };
