b43aa400c081e32c02ff568ad65cadc57bdc386d
Fabien Motte <contact@fabienmotte.com>
2016-02-21 21:36:16 +0100

    WebGL - Update loop optimizations and fix documentation

--- src/components/WebGL/PostProcessing/PostProcessing.js
+++ src/components/WebGL/PostProcessing/PostProcessing.js
@@ -1,37 +1,36 @@
 import { toType } from 'utils';
 
 /**
- * Provides
+ * PostProcessing class
  */
 class PostProcessing {
 
   /**
    * Constructor function
    *
-   * @param  {EffectComposer} effectComposer EffectComposer instance
-   * @param  {Scene}          scene          Scene instance
-   * @param  {Camera}         camera         Camera instance
-   * @param  {Renderer}       renderer       Renderer instance
-   * @param  {GUI}            gui            GUI instance
-   * @param  {Configuration}  configuration  Configuration instance
+   * @param {EffectComposer} EffectComposer EffectComposer instance
+   * @param {Scene}          Scene          Scene instance
+   * @param {Camera}         Camera         Camera instance
+   * @param {Renderer}       Renderer       Renderer instance
+   * @param {GUI}            Gui            GUI instance
+   * @param {Configuration}  Configuration  Configuration instance
    */
-  constructor( effectComposer, scene, camera, renderer, gui, configuration ) {
+  constructor( EffectComposer, Scene, Camera, Renderer, Gui, Configuration ) {
 
-    this.scene = scene;
-    this.camera = camera;
-    this.renderer = renderer;
-    this.configuration = configuration.get( 'postProcessing' );
-    this.passes = this.configuration.passes;
+    this.scene = Scene;
+    this.camera = Camera;
+    this.renderer = Renderer;
+    this.configuration = Configuration.get( 'postProcessing' );
+    this.passes = this.configuration.passes.filter( pass => pass.active );
     this.usePostProcessing = this.configuration.active;
-    this.composer = effectComposer;
-    this.gui = gui;
+    this.composer = EffectComposer;
+    this.gui = Gui;
 
     this.initGUI();
   }
 
   /**
    * InitGUI function
-   * @private
    * @todo Make this function generic
    */
   initGUI() {
@@ -83,8 +82,7 @@ class PostProcessing {
   }
 
   /**
-   * Update function
-   * @public
+   * update function
    */
   update() {
 
@@ -92,9 +90,11 @@ class PostProcessing {
 
       this.composer.reset();
       this.composer.render( this.scene, this.camera );
-      this.passes
-        .filter( pass => pass.active )
-        .forEach( pass => this.composer.pass( pass.constructor ) );
+
+      for ( let i = 0; i < this.passes.length; i++ ) {
+        this.composer.pass( this.passes[ i ].constructor );
+      }
+
       this.composer.toScreen();
 
     } else {
