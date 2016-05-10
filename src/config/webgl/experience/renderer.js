693a0070cb2256c795d7445c891c1bedae8a88d0
Patrick Heng <hengpatrick.pro@gmail.com>
2016-05-10 10:12:25 +0200

    Experience - Fix retina drop of frame

--- src/config/webgl/experience/renderer.js
+++ src/config/webgl/experience/renderer.js
@@ -2,5 +2,5 @@ export default {
   antialias: window.devicePixelRatio <= 1,
   alpha: true,
   clearColor: new THREE.Color( 0xffffff ),
-  pixelRatio: window.devicePixelRatio || 1
+  pixelRatio: 1
 };
