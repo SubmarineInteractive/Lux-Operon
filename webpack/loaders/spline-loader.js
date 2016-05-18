83d724ff7003e02d9c9b0e381163297b68aa578f
Fabien Motte <contact@fabienmotte.com>
2016-05-19 00:26:24 +0200

    Webpack - Fix spline loader linting

--- webpack/loaders/spline-loader.js
+++ webpack/loaders/spline-loader.js
@@ -1,17 +1,17 @@
-import { parseString } from 'xml2js'
-import keyfinder from 'keyfinder'
-import flattenDeep from 'lodash.flattendeep'
+import { parseString } from 'xml2js';
+import keyfinder from 'keyfinder';
+import flattenDeep from 'lodash.flattendeep';
 
 export default function( source ) {
 
   parseString( source, ( err, result ) => {
 
     if( err ) {
-      return this.callback( err )
+      return this.callback( err );
     }
 
-    const data = flattenDeep( keyfinder( result, 'float_array') )[0]['_'].split( ' ' ).map( Number )
+    const data = flattenDeep( keyfinder( result, 'float_array' ) )[ 0 ][ '_' ].split( ' ' ).map( Number );
 
-    this.callback( null, `module.exports = ${JSON.stringify( data )};` )
-  })
+    this.callback( null, `module.exports = ${JSON.stringify( data )};` );
+  });
 }
