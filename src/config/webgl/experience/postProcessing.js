6c075ffc6b796ea1b358a80e5e81117f236a1f2b
Fabien Motte <contact@fabienmotte.com>
2016-06-02 14:04:41 +0200

    Experience - Activate post-processing for production

--- src/config/webgl/experience/postProcessing.js
+++ src/config/webgl/experience/postProcessing.js
@@ -6,7 +6,7 @@ import NoisePass from '@superguigui/wagner/src/passes/noise/noise';
 import FXAAPass from '@superguigui/wagner/src/passes/fxaa/FXAAPass';
 
 export default {
-  active: false,
+  active: ( __PROD__ ) ? true : false,
   passes: [
     {
       name: 'multiPassBloomPass',
